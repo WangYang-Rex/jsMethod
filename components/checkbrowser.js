a1b805384a0601b3db948d04d3a640b38f854af3
wangyang <wangyang@raycloud.com>
2017-06-21 17:41:05 +0800

    add

--- components/checkbrowser.js
+++ components/checkbrowser.js
@@ -1,21 +1,32 @@
-;function checkBrowser() {
+
+;/**
+ * this function is to check Browser
+ * if IE,it will return IE version
+ * @returns
+ * {
+ *   "isIE": true,
+ *   "fIEVersion": 8
+ * }
+ */
+var checkBrowser = function() {
     var userAgent = navigator.userAgent; //取得浏览器的userAgent字符串
     var isOpera = userAgent.indexOf("Opera") > -1; //判断是否Opera浏览器
     var isIE = userAgent.indexOf("compatible") > -1 && userAgent.indexOf("MSIE") > -1 && !isOpera; //判断是否IE浏览器
     var isFF = userAgent.indexOf("Firefox") > -1; //判断是否Firefox浏览器
     var isChrome = userAgent.indexOf("Chrome") > -1; //判断是否Firefox浏览器
     var isSafari = userAgent.indexOf("Safari") > -1; //判断是否Safari浏览器
+    var fIEVersion = '';
     if (isIE) {
         var IE5 = IE55 = IE6 = IE7 = IE8 = false;
         var reIE = new RegExp("MSIE (\\d+\\.\\d+);");
         reIE.test(userAgent);
-        var fIEVersion = parseFloat(RegExp["$1"]);
-        return {
-            "isIE": isIE,
-            "fIEVersion": fIEVersion
-        }
-    }//isIE end
+        fIEVersion = parseFloat(RegExp["$1"]);
+
+    } //isIE end
     return {
-        "isIE": isIE
+        "isIE": isIE,
+        "fIEVersion": fIEVersion
     }
 }
+
+module.exports = checkBrowser;
